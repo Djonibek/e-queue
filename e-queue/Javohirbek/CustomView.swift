13a1e2267d66ddf94871b63ca5c668fccc959831
Javohirbek <javohirbekmuhammadiyev1402@gmail.com>
2023-01-23 19:50:42 +0500

    -customView

---
+++ e-queue/Javohirbek/CustomView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomView.swift
+//  e-queue
+//
+//  Created by Javohirbek Muhammadiyev on 23/01/23.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
